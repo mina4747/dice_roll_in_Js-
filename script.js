6f70b65dd8dccc26eb043d4215beff646a910bd4
Mina <minawilliam@gmail.com>
2024-08-16 00:57:31 +0300

    initial idea

---
+++ script.js
@@ -0,0 +1,26 @@
+
+function roll() {
+
+
+    switch (Math.ceil(Math.random() * 100 % 6)) {
+        case 1:
+            document.getElementById("dice").innerHTML = ` <img src="side_1_pip.png" alt="1" width="200" height="200">`;
+            break;
+        case 2:
+            document.getElementById("dice").innerHTML = ` <img src="side_2_pips.png" alt="1" width="200" height="200">`;
+            break;
+        case 3:
+            document.getElementById("dice").innerHTML = ` <img src="side_3_pips.png" alt="1" width="200" height="200">`;
+            break;
+        case 4:
+            document.getElementById("dice").innerHTML = ` <img src="side_4_pips.png" alt="1" width="200" height="200">`;
+            break;
+        case 5:
+            document.getElementById("dice").innerHTML = ` <img src="side_5_pips.png" alt="1" width="200" height="200">`;
+            break;
+        case 6:
+            document.getElementById("dice").innerHTML = ` <img src="side_6_pips.png" alt="1" width="200" height="200">`;
+            break;
+    }
+
+}
